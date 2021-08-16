5ec5cc6e9dd87baa21c7188581a229f7c601cb98
chuntese <jxie0004@student.monash.edu>
2021-08-16 11:25:40 +0800

    query to obj

--- qsparser/__init__.py
+++ qsparser/__init__.py
@@ -12,9 +12,9 @@ def stringify(obj: dict[str, Any]) -> str:
         valueArray.append(i)
     for i in range(len(valueArray)):
         if type(valueArray[i]) == list:
-            result = stringArray(keysArray[i],valueArray[i],result)
+            result = string_array(keysArray[i],valueArray[i],result)
         elif type(valueArray[i]) == dict:
-            result = objectArray(keysArray[i],valueArray[i],result)
+            result = object_array(keysArray[i],valueArray[i],result)
         elif type(valueArray[i]) == bool:
             result += '%s=' % keysArray[i] + str(valueArray[i]).lower()
         else:
@@ -30,26 +30,67 @@ def stringify(obj: dict[str, Any]) -> str:
     return result
 
 
-def stringArray(key,array,result):
+def string_array(key: str, array: list, result: str) -> str:
     for i in range(len(array)):
         if type(array[i]) == list and len(array[i]) > 1:
-            result = stringArray('%s[%s]'% (key,i),array[i],result)
+            result = string_array('%s[%s]'% (key,i),array[i],result)
         else:
             result += '%s[%s]=%s&' % (key,i,array[i])
     return result
 
-def objectArray(key,object,result):
+
+def object_array(key: str, object: dict, result: str) -> str:
     for i in object.keys():
         if type(object.get(i)) == dict:
-            result = objectArray('%s[%s]' % (key,i),object.get(i),result)
+            result = object_array('%s[%s]' % (key,i),object.get(i),result)
         else:
             result += '%s[%s]=%s&' % (key,i,object.get(i))
     return result
 
+
 def parse(qs: str) -> dict[str, Any]:
-    return {}
+    qs = qs.replace('[',',')
+    qs = qs.replace(']','')
+
+
+    tempObj = {}
+    result = {}
+    querys = re.split('[& =]',qs)
+
+    for i in range(len(querys)):
+        if i % 2 == 0:
+            tempObj.update({querys[i]:urllib.parse.unquote(querys[i+1])})
+
+
+    for i in tempObj.items():
+        if i[1] == 'true':
+            tempObj.update({i[0]:True})
+        if i[1] == 'false':
+            tempObj.update({i[0]:False})
+        if '%20' in i[1]:
+            tempObj.update({i[0]:i[1].replace('%20',' ')})
+
+        if len(i[0]) > 1:
+            tempObj = mutiObj(i)
+            for j in tempObj.keys():
+                if j in result:
+                    result.update({j:{**result.get(j),**tempObj.get(j)}})
+                else:
+                    result.update({j:tempObj.get(j)})
+    for i in tempObj.keys():
+        if len(i) < 2:
+            result.update({i:tempObj.get(i)})
+    return result
+
+def mutiObj(query):
+    key = query[0].split(',')
+
+    for i in range(len(key)-1):
+        temp = {key[i]:{key[i+1]:query[1]}}
+    return temp
+
 
 if __name__ == '__main__':
-    print(stringify({' a':'俊','c':'d'}))
+    print(parse('a[0]=1&a[1]=2&a[2]=3&b[0]=q&b[1]=w&b[2]=%E4%BF%8A'))
 
 
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -19,5 +19,47 @@ class TestParse(TestCase):
     def test_parse_decodes_true_into_string(self):
         qs = 'a=true'
         result = parse(qs)
-        expected = {'a': 'true'}
+        expected = {'a': True}
+        self.assertEqual(result, expected)
+
+    def test_parse_decodes_false_into_string(self):
+        obj = 'a=false'
+        result = parse(obj)
+        expected = {'a': False}
+        self.assertEqual(result, expected)
+
+    def test_parse_decodes_string_into_string(self):
+        obj = 'a=b'
+        result = parse(obj)
+        expected = {'a': 'b'}
+        self.assertEqual(result, expected)
+
+    def test_parse_decodes_whitespaces(self):
+        obj = 'a=b%20c'
+        result = parse(obj)
+        expected = {'a': 'b c'}
+        self.assertEqual(result, expected)
+
+    def test_parse_decodes_special_chars(self):
+        obj = 'a=%E4%BF%8A'
+        result = parse(obj)
+        expected = {'a': '俊'}
+        self.assertEqual(result, expected)
+
+    def test_parse_concats_multiple_items_with_the_ampersand(self):
+        obj = 'a=b&c=d'
+        result = parse(obj)
+        expected = {'a': 'b', 'c': 'd'}
+        self.assertEqual(result, expected)
+
+    def test_parse_encodes_dict_into_multiple_entries(self):
+        obj = 'a[b]=c&d[e]=f&d[g]=h'
+        result = parse(obj)
+        expected = {'a': {'b': 'c'}, 'd': {'e': 'f', 'g': 'h'}}
+        self.assertEqual(result, expected)
+
+    def test_parse_encodes_list_into_multiple_entries(self):
+        obj = 'a[0]=1&a[1]=2&a[2]=3&b[0]=q&b[1]=w&b[2]=e'
+        result = parse(obj)
+        expected = {'a': {'0': '1', '1': '2', '2': '3'}, 'b': {'0':'q', '1':'w', '2':'e'}}
         self.assertEqual(result, expected)
