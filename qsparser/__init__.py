bd9c8bcee154603d86cf7f965671a96b0303a268
chuntese <jxie0004@student.monash.edu>
2021-08-17 15:13:11 +0800

    better stringify

--- qsparser/__init__.py
+++ qsparser/__init__.py
@@ -1,51 +1,38 @@
 from typing import Any
-from urllib.parse import quote,unquote
-from re import split,search
+from urllib.parse import quote, unquote
+from re import split
+
 
 def stringify(obj: dict[str, Any]) -> str:
-    result = ''
-    keysArray = []
-    valueArray = []
-    for i in obj.keys():
-        keysArray.append(i)
-    for i in obj.values():
-        valueArray.append(i)
-    for i in range(len(valueArray)):
-        if type(valueArray[i]) == list:
-            result = string_array(keysArray[i],valueArray[i],result)
-        elif type(valueArray[i]) == dict:
-            result = object_array(keysArray[i],valueArray[i],result)
-        elif type(valueArray[i]) == bool:
-            result += '%s=' % keysArray[i] + str(valueArray[i]).lower()
-        else:
-            result += '%s=%s&' % (keysArray[i],valueArray[i])
-
-    if result[len(result)-1] == '&':
-        result = result[:-1]
-
-    result = result.replace(" ","%20")
-    for i in range(len(result)):
-        if search(u'[\u4e00-\u9fff]', result[i]):
-            result = result.replace(result[i],quote(result[i]))
-    return result
+    tokens: list[str] = []
+    for key, value in obj.items():
+        tokens.extend(gen_tokens([key], value))
+    return '&'.join(tokens)
 
 
-def string_array(key: str, array: list, result: str) -> str:
-    for i in range(len(array)):
-        if type(array[i]) == list and len(array[i]) > 1:
-            result = string_array('%s[%s]'% (key,i),array[i],result)
-        else:
-            result += '%s[%s]=%s&' % (key,i,array[i])
-    return result
+def gen_tokens(items: list[str], value: Any) -> list[str]:
+    if value is True:
+        return [f'{gen_key(items)}=true']
+    elif value is False:
+        return [f'{gen_key(items)}=false']
+    elif value is None:
+        return [f'{gen_key(items)}=null']
+    elif isinstance(value, list):
+        result: list[str] = []
+        for i, v in enumerate(value):
+            result.extend(gen_tokens(items + [str(i)], v))
+        return result
+    elif isinstance(value, dict):
+        result: list[str] = []
+        for k, v in value.items():
+            result.extend(gen_tokens(items + [str(k)], v))
+        return result
+    else:
+        return [f'{gen_key(items)}={quote(str(value))}']
 
 
-def object_array(key: str, object: dict, result: str) -> str:
-    for i in object.keys():
-        if type(object.get(i)) == dict:
-            result = object_array('%s[%s]' % (key,i),object.get(i),result)
-        else:
-            result += '%s[%s]=%s&' % (key,i,object.get(i))
-    return result
+def gen_key(items: list[str]) -> str:
+    return f'{items[0]}[{"][".join(items[1:])}]'.removesuffix('[]')
 
 
 def parse(qs: str) -> dict[str, Any]:
@@ -67,9 +54,3 @@ def assign_to_result(result: dict[str, Any], items: list[str], value: str) -> di
         result[items[0]] = {}
     assign_to_result(result[items[0]], items[1:], value)
     return result
-
-
-if __name__ == '__main__':
-    print(parse('a[b][c]=d&d[e]=f&d[g]=h'))
-
-
